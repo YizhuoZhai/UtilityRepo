f06daf15438a606428b069f7b7429f7b4c3c3ead
Yizhuo <yizh.zhai@gmail.com>
2020-07-05 17:55:44 -0700

    Create getFiles.py

---
+++ getFiles.py
@@ -0,0 +1,15 @@
+# get file name of specific suffix
+import sys
+import os.path
+file_abs = ""
+def getFiles(dir_name):
+    for dirpath, dirnames, filenames in os.walk(dir_name):    
+        for filename in filenames:        
+            if os.path.splitext(filename)[1] == ".bc":            
+                module = os.path.join(dirpath, filename)            
+                with open(file_abs, "a") as f:                
+                    f.write(module)                
+                    f.write("\n")
+
+if __name__ == "__main__": 
+    getDesp(sys.argv[1])
